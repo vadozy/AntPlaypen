25c2fc7ff824a034f43388cbd7d97c081abed45e
Vadim Storozhuk <vadim.storo@gmail.com>
2016-05-01 23:13:20 -0400

    Initial Commit

---
+++ src/my/ant/Main.java
@@ -0,0 +1,14 @@
+package my.ant;
+
+/**
+ * Entry point
+ * Created by vadim on 5/1/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello from ant");
+
+    }
+}
